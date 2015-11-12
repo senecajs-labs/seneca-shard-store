cad78761cac030b6613ff37561f8a856b38e8089
Paolo Chiodi <chiodi84@gmail.com>
2015-11-12 12:54:32 +0100

    linted index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
  *
  * Based on seneca-jsonfile-store
  * Copyright (c) 2013 Richard Rodger, MIT License */
-"use strict";
+'use strict'
 
 var Sharder = require('sharder')
 var async = require('async')
 var Seneca = require('seneca')
-var _ = require('underscore');
+var _ = require('underscore')
 var name = 'shard-store'
 
 
 module.exports = function (opts) {
-  var seneca = this;
+  var seneca = this
 
 
   /**
@@ -24,30 +24,28 @@ module.exports = function (opts) {
    */
   var shards
 
-  function configure(specification, cb) {
-
-
+  function configure (specification, cb) {
     for (var shardId in specification.shards) {
       var shard = specification.shards[shardId]
       shard.seneca = Seneca()
-      shard.seneca.use(shard.store.plugin, shard.store.options);
+      shard.seneca.use(shard.store.plugin, shard.store.options)
     }
 
     shards = new Sharder(specification)
 
-    cb(null, shards);
-
+    cb(null, shards)
   }
 
-  function shardWrap(args, cb) {
+  function shardWrap (args, cb) {
     var seneca = this
 
     var id
-      , shard
+    var shard
 
     if (args.ent) {
       id = args.ent.id$
-    } else if (args.q) {
+    }
+    else if (args.q) {
       id = args.q.id
     }
 
@@ -68,37 +66,33 @@ module.exports = function (opts) {
     if (args.zone) {
       for (var sh in shards._appendShards) {
         if (shards._appendShards[sh].zone === args.zone) {
-          shard = shards._appendShards[sh];
+          shard = shards._appendShards[sh]
         }
       }
     }
 
-
     shard.seneca.act(args, cb)
   }
 
-  function shardWrapAll(args, cb) {
-    var seneca = this
-    var skip,limit
+  function shardWrapAll (args, cb) {
+    var skip
+    var limit
 
     async.concat(Object.keys(shards.shards), function (shardId, cb) {
       var shard = shards.shards[shardId]
 
-      if(args.q && void 0!== args.q.limit$  ){
-
-        if(!limit)
-          limit= args.q.limit$
+      if (args.q && void 0 !== args.q.limit$) {
+        if (!limit) {
+          limit = args.q.limit$
+        }
       }
 
 
-      if(args.q && void 0!== args.q.skip$){
-
-        skip= args.q.skip$
-        args.q.limit$= args.q.limit$+skip
+      if (args.q && void 0 !== args.q.skip$) {
+        skip = args.q.skip$
+        args.q.limit$ = args.q.limit$ + skip
         delete args.q.skip$
       }
-
-
 
       shard.seneca.act(args, function (err, result) {
         if (err) {
@@ -107,11 +101,10 @@ module.exports = function (opts) {
         cb(undefined, result)
       })
     }, function (err, result) {
-
-
       if (!err && result) {
         merge(args, result)
       }
+
       if (args.cmd === 'list' && args.q) {
         var startindex = 0
         var endindex = result.length
@@ -124,19 +117,19 @@ module.exports = function (opts) {
 
         if (limit) {
           limitOrSkip = true
-          endindex = result.length > limit+startindex ?limit+startindex : result.length
+          endindex = result.length > limit + startindex ? limit + startindex : result.length
         }
 
-        if (limitOrSkip)
-        {
+        if (limitOrSkip) {
           result = result.slice(startindex, endindex)
         }
       }
-      if ((args.cmd === 'save' || args.cmd === 'load') && result.length == 0) {
-        result = null;
+
+      if ((args.cmd === 'save' || args.cmd === 'load') && result.length === 0) {
+        result = null
       }
 
-      cb(err, result);
+      cb(err, result)
     })
   }
 
@@ -162,31 +155,29 @@ module.exports = function (opts) {
   /**
    * initialization
    */
-  var meta = seneca.store.init(seneca, opts, store);
-  var desc = meta.desc;
-  seneca.add({init: store.name, tag: meta.tag}, function (args, done) {
+  var meta = seneca.store.init(seneca, opts, store)
+  var desc = meta.desc
+  seneca.add({ init: store.name, tag: meta.tag }, function (args, done) {
     configure(opts, function (err) {
       if (err) {
-        return seneca.fail({code: 'entity/configure', store: store.name, error: err, desc: desc}, done);
+        return seneca.fail({ code: 'entity/configure', store: store.name, error: err, desc: desc }, done)
       }
-      else done();
-    });
-  });
+      else done()
+    })
+  })
 
-  return {name: store.name, tag: meta.tag};
-
+  return { name: store.name, tag: meta.tag }
 }
 
 
-function merge(args, list) {
+function merge (args, list) {
   distinct(args, list)
   sort(args, list)
 }
 
-function distinct(args, list) {
+function distinct (args, list) {
   if (args && args.cmd === 'list' && args.q && args.q.distinct$) {
     if (_.isArray(list)) {
-      var distinct = args.q.distinct$
       var index = 0
       var visited = {}
       while (index < list.length) {
@@ -195,7 +186,8 @@ function distinct(args, list) {
         if (!visited.hasOwnProperty(JSON.stringify(item))) {
           visited[JSON.stringify(item)] = true
           index++
-        } else {
+        }
+        else {
           list.splice(index, 1)
         }
       }
@@ -203,31 +195,34 @@ function distinct(args, list) {
   }
 }
 
-function sort(args, list) {
+function sort (args, list) {
   if (args && args.cmd === 'list' && args.q && args.q.sort$) {
     if (_.isArray(list)) {
       list.sort(function (a, b) {
         if (!a && !b) {
           return 0
-        } else if (a && !b) {
+        }
+        else if (a && !b) {
           return 1
-        } else if (!a && b) {
+        }
+        else if (!a && b) {
           return -1
-        } else {
+        }
+        else {
           for (var sortAttr in args.q.sort$) {
             if (a[sortAttr] === b[sortAttr]) {
               continue
-            } else if (a[sortAttr] < b[sortAttr]) {
+            }
+            else if (a[sortAttr] < b[sortAttr]) {
               return +args.q.sort$[sortAttr]
-            } else {
+            }
+            else {
               return -args.q.sort$[sortAttr]
             }
           }
           return 0
         }
       })
-
     }
   }
-
 }
